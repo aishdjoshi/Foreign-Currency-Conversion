7a533543851fc9af2e7e9d87f41c40a00299b708
Aishwarya Joshi <aishdjoshi@gmail.com>
2020-11-21 18:25:02 +0530

    Third Commit

--- src/test/java/runner/RunFeature.java
+++ src/test/java/runner/RunFeature.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/feature/functionalTests.feature",
+        features = "src/test/resources/feature/functionalTests.feature",
         glue = {"stepDefinition"}
 )
 
